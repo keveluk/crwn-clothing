e3eb1736efa0d1ee4e53f77bda63b34a977f1ca9
keveluk <carlos.stefano@hotmail.com.br>
2022-07-17 03:15:16 -0300

    updating our directory page

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+import { unhover } from '@testing-library/user-event/dist/hover';
+import './category-item.styles.scss';
+
+const CategoryItem = ({category}) => {
+    const {title,imageUrl} = category;
+  return (    
+        <div className='category-container'>
+          <div className="background-image" style={{
+            backgroundImage:`url(${imageUrl})`
+          }}/>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>      
+  )
+}
+
+export default CategoryItem
